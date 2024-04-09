0e96ee7b70e36b604150e18374808c6dd04156c7
AleandroPresta <all96p@libero.it>
2024-04-09 11:17:42 +0200

    Working object detection with a single image already given. Working towards letting the user load the image

--- src/index.js
+++ src/index.js
@@ -17,4 +17,38 @@ const cocoSsd = require('@tensorflow-models/coco-ssd');
 
   console.log('Predictions: ');
   console.log(predictions);
+
+  const image = document.getElementById("img-1");
+
+  for (const detection of predictions){
+      drawBoundingBoxes(image, detection);
+  }
+
 })();
+
+function drawBoundingBoxes(image, detection) {
+  const canvas = document.createElement("canvas");
+  canvas.width = image.width;
+  canvas.height = image.height;
+
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(image, 0, 0);
+
+  ctx.beginPath();
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = 'red';  
+
+  const [x, y, width, height] = detection.bbox;
+  const className = detection.class;
+  const accuracy = detection.score;
+
+  ctx.rect(x, y, width, height);
+  ctx.stroke();
+
+  const text = `${className} (${(accuracy * 100).toFixed(2)}%)`;
+  ctx.font = '48px serif'; // Set font size and family
+  ctx.fillStyle = 'red';
+  ctx.fillText(text, x, y);
+
+  image.src = canvas.toDataURL(); // Convert canvas to data URL
+}
